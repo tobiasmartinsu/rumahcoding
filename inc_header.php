50576c794b65f9841349bc33a2b76809d88253bb
tobiasmartinsu <111673503+tobiasmartinsu@users.noreply.github.com>
2022-10-04 17:54:34 +0700

    Update inc_header.php

--- inc_header.php
+++ inc_header.php
@@ -10,7 +10,7 @@ include_once("inc/inc_function.php");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>bootcamp</title>
-    <link rel="stylesheet" href="<?php echo url_dasar()?>/css/style.css">
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
     <nav>
